ee6e08811c8d746d55820bf1ffa7c1f68fa78cfb
gsandaka <gsandaka@host.docker.internal>
2020-09-26 17:56:19 +0530

    jenkins

---
+++ jenkinsM6/src/main/java/com/dxc/Jenkins.java
@@ -0,0 +1,12 @@
+package com.dxc;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello friends from DXC Technology");
+		
+		System.out.println("Hello DXC..");
+	}
+
+}
